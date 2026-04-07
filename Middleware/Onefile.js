63c07831fa80f7d89e03c80623ed5241bbb440e7
Rajendra Acharya <acharyarajendra48@gmail.com>
2026-04-07 19:19:02 +0530

    after fixing the node error 11

--- Middleware/Onefile.js
+++ Middleware/Onefile.js
@@ -7,7 +7,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,            
-  secure: true, 
+  secure: false, 
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
